91f812f3f75f46b3d294b3af7693716ad19c402f
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-04-16 00:35:48 +0330

    Add some global files

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule IgniterCss.MixProject do
     [
       app: :igniter_css,
       version: "0.0.1",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
